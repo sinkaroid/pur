505a359669f6beeb631dfe98bc32913e75e45573
sinkaroid <bibsindra@gmail.com>
2022-09-23 20:43:26 +0700

    fix: simplified dark mode

--- app.vue
+++ app.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import Toggle from '@vueform/toggle';
-import { useState } from '#app';
-import { onMounted, watch } from '@vue/runtime-core';
-type Theme = 'light' | 'dark';
-const LOCAL_STORAGE_THEME_KEY = 'theme';
-const darkMode = useState('theme', () => false);
 
 useHead({
   titleTemplate: (titleChunk) => {
@@ -18,49 +12,19 @@ useHead({
   ]
 })
 
-const setTheme = (newTheme: Theme) => {
-  localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-  darkMode.value = newTheme === 'dark';
-};
-onMounted(() => {
-  const isDarkModePreferred = window.matchMedia(
-    '(prefers-color-scheme: dark)'
-  ).matches;
-  const themeFromLocalStorage = localStorage.getItem(
-    LOCAL_STORAGE_THEME_KEY
-  ) as Theme;
-  if (themeFromLocalStorage) {
-    setTheme(themeFromLocalStorage);
-  } else {
-    setTheme(isDarkModePreferred ? 'dark' : 'light');
-  }
-});
-watch(darkMode, selected => {
-  setTheme(selected ? 'dark' : 'light');
-});
 </script>
 <style src="@vueform/toggle/themes/default.css"></style>
 <template>
 
-  <div>
-              <div
-    :class="{
-      'theme-light': !darkMode,
-      'theme-dark': darkMode,
-    }"
-    class="h-screen bg-themeBackground p-5"
-  >
 
 
-  
+
+  <div id="app" class="min-h-screen">
     <NuxtLayout>
   
       <NuxtPage/>
       
     </NuxtLayout>
-  
-
-  </div>
-  </div>
+</div>
   
 </template>
