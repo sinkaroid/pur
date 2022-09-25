46c5faec82e971f2551da330709fb99f5d39cf5e
sinkaroid <bibsindra@gmail.com>
2022-09-25 11:42:02 +0700

    pur

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
 <footer>
-<<<<<<< HEAD
   
   <div class="container mx-auto">
     <p class="text-gray-700 text-center text-sm pb-5 pt-5 ">
@@ -8,10 +7,6 @@
       <a href="https://github.com" target="_blank" class="text-blue-500 hover:text-blue-600 underline">Anam</a>
     </p>
      <a class="gotopbtn" href="#">⬆️</a>
-=======
-  <div class="container mx-auto ">
-    <p class="text-gray-700 text-center text-sm pb-5 pt-5 ">Created with Nuxt & Vuejs</p>
->>>>>>> 3b7afdc61e65700ccdbc4543d1136839831e03f6
   </div>
 </footer>
 </template>
