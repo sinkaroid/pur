5a3042c5ad4d6b9079199b4e1e56e20b685e3943
几句レし <namdevel@gmail.com>
2022-09-26 20:28:40 +0700

    feat: remove 2D name :angry:

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -22,7 +22,7 @@ const toggleDark = useToggle(isDark)
 <template>
   <nav class="fixed top-0 z-10 w-full bg-white dark:bg-neutral-900">
     <div class="container py-3 md:py-0 max-w-3xl mx-auto flex flex-row items-center justify-between px-5 lg:px-0 ">
-      <a title="Home" class="text-xl md:text-2xl font-bold no-underline dark:text-green-500" href="/">ELAINA</a>
+      <a title="Home" class="text-xl md:text-2xl font-bold no-underline dark:text-green-500" href="/">NAMDEVEL</a>
       <ul class="text-sm md:text-base flex list-none  m-0 p-0">
         <li>
           <a title="About Us" class="px-2 md:px-4 block py-2 md:py-4 font-bold no-underline dark:text-green-500" href="/about">About</a>
