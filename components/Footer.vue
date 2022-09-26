af4476bbe4d27cc405afb0359b24c8215e2bf9e2
几句レし <namdevel@gmail.com>
2022-09-26 12:57:19 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,10 @@
   <div class="container mx-auto">
     <p class="text-gray-700 text-center text-sm pb-5 pt-5 ">
       &copy; MIT License
-      <a href="https://github.com" target="_blank" class="text-blue-500 hover:text-blue-600 underline">Anam</a>
+      <a href="https://github.com" target="_blank" class="text-blue-500 hover:text-blue-600 underline float-left">Anam</a>
+      <a href="https://github.com" target="_blank" class="text-blue-500 hover:text-blue-600 underline float-right">Github</a>
     </p>
-     <a class="gotopbtn" href="#">⬆️</a>
+    <a href="#" class="gotopbtn bg-white border-3 light:border-neutral-600 dark:border-3 border-green-500">🔝</a>
   </div>
 </footer>
 </template>
