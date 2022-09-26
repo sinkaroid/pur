064805a9d515b2dc8dcffe410568fa801ae71c93
几句レし <namdevel@gmail.com>
2022-09-26 20:27:56 +0700

    fix: sticky padding

--- pages/client.vue
+++ pages/client.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TabPane from '~~/components/Tabs/TabPane.vue';
+  
 import TabContent from '~~/components/Tabs/TabContent.vue';
 import { useProjectsStore } from '~~/store/data/projects';
 definePageMeta({
@@ -18,7 +18,7 @@ console.log(store.projects)
 </script>
 <template>
 <section class="content">
-  <div class="container mx-auto max-w-[75ch]!">
+  <div class="container mx-auto max-w-[65ch]! mt-25">
     <div class="prose">
       <h1 class="text-center">Clients</h1>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br><br>
@@ -42,7 +42,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
               :title="project.title"
               :image="project.image"
               :description="project.description"
-              class="w-full"
+              class="w-full bg-cover"
               >
             </Card>
           </div>
